0dae41cdd78fa7c79c9fa675c7a8fe58c80259d4
DavyAndrade <davy.dama55@gmail.com>
2025-08-16 12:37:46 -0300

    style: Navbar Mobile-First
    Implementando e corrigindo estilos e dropdown

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,10 @@
 console.log("Script loaded successfully!");
 
 function exibirDropdown() {
-  var x = document.getElementById("links");
+  var x = document.getElementById("navbar__dropdown");
   if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
     x.style.display = "flex";
-    x.style.flexDirection = "column";
-    x.style.gap = "1rem"
-    x.style.position = "absolute";
-    x.style.top = "64px";
-    x.style.left = "0";
-    x.style.width = "100%";
-    x.style.padding = "1rem";
-    x.style.backgroundColor = "#fff";
   }
 }
