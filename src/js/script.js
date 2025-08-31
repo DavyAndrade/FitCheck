28cfae95b244da86be3bc806486f07ee638af2cd
DavyAndrade <davy.dama55@gmail.com>
2025-08-31 13:26:20 -0300

    feat: Responsividade da Navbar
    Adaptado para Tablet, Notebook, Desktop e telas maiores

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ window.addEventListener("load", () => {
 
 // Navbar Dropdown
 function toggleDropdown() {
-  const dropdownMenu = document.getElementById("navbar-dropdown");
+  const dropdownMenu = document.getElementById("dropdownMenu");
   if (dropdownMenu.style.display === "flex") {
     dropdownMenu.style.display = "none";
   } else {
@@ -18,26 +18,26 @@ function toggleDropdown() {
 // Navbar Component
 function loadNavbar() {
   return `
-  <nav class="navbar" id="navbar-mobile">
+          <nav class="navbar">
             <a href="index.html" class="navbar__logo">
                 <i class="fa-solid fa-heart-pulse"></i> FitCheck
             </a>
 
-            <button class="navbar__toggle" onclick="toggleDropdown()" aria-expanded="false"
-                aria-controls="navbar-dropdown">
+            <!-- Apenas Mobile -->
+            <button class="navbar__toggle" onclick="toggleDropdown()" aria-expanded="false" aria-controls="dropdownMenu"
+                id="mobile">
                 <span class="sr-only">Abrir menu</span>
                 <i class="fa-solid fa-bars" aria-hidden="true"></i>
             </button>
 
-            <!-- Dropdown -->
-            <div class="navbar__dropdown" id="navbar-dropdown">
-                <div class="navbar__links">
-                    <a href="index.html" class="navbar__link">Início</a>
-                    <a href="imc.html" class="navbar__link">Calculadora IMC</a>
-                    <a href="pci.html" class="navbar__link">Calculadora PCI</a>
-                </div>
+            <div class="navbar__dropdown" id="dropdownMenu">
+                <ul class="navbar__links" id="mobile">
+                    <li class="navbar__item"><a href="index.html" class="navbar__link">Início</a></li>
+                    <li class="navbar__item"><a href="imc.html" class="navbar__link">Calculadora IMC</a></li>
+                    <li class="navbar__item"><a href="pci.html" class="navbar__link">Calculadora PCI</a></li>
+                </ul>
 
-                <div class="navbar__soon">
+                <div class="navbar__soon" id="mobile">
                     <p class="navbar__soon-title">Em breve:</p>
                     <ul class="navbar__soon-container">
                         <li class="navbar__soon-item">Taxa Metabólica Basal</li>
@@ -45,6 +45,23 @@ function loadNavbar() {
                     </ul>
                 </div>
             </div>
+
+            <!-- Apenas Desktop -->
+            <ul class="navbar__links" id="desktop">
+                <li class="navbar__item"><a href="index.html" class="navbar__link">Início</a></li>
+                <li class="navbar__item"><a href="imc.html" class="navbar__link">Calculadora IMC</a></li>
+                <li class="navbar__item"><a href="pci.html" class="navbar__link">Calculadora PCI</a></li>
+                <div class="navbar__dropdown--desktop">
+                    <li class="navbar__item" tabindex="0">Mais cálculos <i class="fa-solid fa-chevron-down"></i></li>
+                    <div class="navbar__dropdown-content">
+                        <ul class="navbar__dropdown-list">
+                            <p class="navbar__soon-title">Em breve:</p>
+                            <li class="navbar__dropdown-item">• Taxa Metabólica Basal</li>
+                            <li class="navbar__dropdown-item">• Calculadora de Hidratação</li>
+                        </ul>
+                    </div>
+                </div>
+            </ul>
         </nav>
         `;
 }
